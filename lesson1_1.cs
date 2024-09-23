b9d5dcc20c8e95d1af8f555b13aebe2591af4235
kozeeva-aa <mopedkafki@gmail.com>
2024-09-23 15:14:15 +0300

    Добавьте файлы проекта.

---
+++ lesson1_1.cs
@@ -0,0 +1,24 @@
+﻿// Запросить ввод с консоли: Имя, Фамилия, Отчество человека и как только будет введено последнее значение,
+// вывести на экран всё в одну строчку
+using System;
+namespace lesson1;
+
+class Program
+{ 
+    
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите имя ");
+        string name = Console.ReadLine();
+
+        Console.WriteLine("Введите отчество ");
+        string middlename = Console.ReadLine();
+
+        Console.WriteLine("Введите фамилию ");
+        string surname = Console.ReadLine();
+
+        Console.WriteLine($"{name} {middlename} {surname}");
+        Console.ReadKey();
+    }
+}
+
